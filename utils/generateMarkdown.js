c435677fa7b6e8037414fec2e97527b74125da4c
Jasmine <dr.jingyuezhao@outlook.com>
2023-02-19 15:48:04 +0000

    update the license badge links/url addresses

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,25 +2,25 @@
 function renderBadge(licenseChoice) {
   switch (licenseChoice) {
     case 'Apache License 2.0':
-      return '![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)';
+      return '![License: Apache 2.0](https://img.shields.io/badge/License-Apache_2.0-blue.svg)';
       break;
     case 'Boost Software License 1.0':
-      return '![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)';
+      return '![License: Boost 1.0](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)';
       break;
     case 'BSD 3-Clause "New" or "Revised" license':
-      return '![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)';
+      return '![License: BSD 3-Clause](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)';
       break;
     case 'GNU General Public License v2.0':
-      return '![License](https://img.shields.io/badge/License-GPL_v2-blue.svg)';
+      return '![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)';
       break;
     case 'MIT license':
-      return '![License](https://img.shields.io/badge/License-GPL_v2-blue.svg)';
+      return '![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)';
       break;
     case 'Mozilla Public License 2.0':
-      return '![License](https://img.shields.io/badge/License-GPL_v2-blue.svg)';
+      return '![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)';
       break;
     case 'The Unlicense':
-      return '![License](https://img.shields.io/badge/License-GPL_v2-blue.svg)';
+      return '![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)';
       break;
   }
 }
